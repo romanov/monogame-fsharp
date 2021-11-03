fb676d73ef58940060840e50e6eee6871e2cb335
romanov <romanov@users.noreply.github.com>
2021-11-03 16:38:31 +0300

    Initial net6 support

---
+++ Game1.fs
@@ -0,0 +1,45 @@
+﻿module Game
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+
+type Game1 () as x =
+    inherit Game()
+ 
+    do x.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(x)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    override x.Initialize() =
+    
+        spriteBatch <- new SpriteBatch(x.GraphicsDevice)
+        base.Initialize()
+         
+         // TODO: Add your initialization logic here
+
+        ()
+
+    override this.LoadContent() =
+        
+         // TODO: use x.Content to load your game content here   
+         // On Windows you can load any PNG file directly as Texture2D
+
+         // Read more about MonoGame's Content Pipeline: https://docs.monogame.net/articles/tools/mgcb_editor.html
+         // or install it with package manager console: [dotnet tool install -g dotnet-mgcb-editor]
+        
+        ()
+ 
+    override this.Update (gameTime) =
+
+         // TODO: Add your update logic here
+        
+        ()
+ 
+    override this.Draw (gameTime) =
+
+        x.GraphicsDevice.Clear Color.CornflowerBlue
+        
+        // TODO: Add your drawing code here
+
+        ()
---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Game
+
+[<EntryPoint>]
+let main argv = 
+    use g = new Game1()
+    g.Run()
+    0
